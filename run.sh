f75280f7d8ff2d6754bf848bbb3c11bee592e87f
Manoj Surudwad <manoj.surudwad@trilio.io>
2021-02-02 18:19:42 +0530

    kind install

--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
-tar -cvzf preflight.tar.gz tools/preflight/preflight
+set -ex
+
+helm repo add k8s-triliovault-stable http://charts.k8strilio.net/trilio-stable/k8s-triliovault
+helm repo add k8s-operator-stable http://charts.k8strilio.net/trilio-stable/k8s-triliovault-operator
+
+helm install -n ms k8s-tv-operator k8s-operator-stable/k8s-triliovault-operator --version v0.2.6
+kubectl apply -n ms -f tvm.yaml
+
+kubectl get -n ms -f tvm.yaml -oyaml
+
+set +ex
