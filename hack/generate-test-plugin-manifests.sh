71dce1f7e42c90a9690b27a53b6c488da47cf521
Manoj Surudwad <manoj.surudwad@trilio.io>
2021-02-08 16:19:03 +0530

    krew workflow added

--- hack/generate-test-plugin-manifests.sh
+++ hack/generate-test-plugin-manifests.sh
@@ -20,9 +20,13 @@ while IFS= read -r -d $'\0' f; do
   TZ=UTC touch -mt "0001010000" "$f"
 done < <(find $build_dir -print0)
 
-cp .krew/preflight.yaml $build_dir/preflight.yaml
-tar_checksum="$(cut -d' ' -f1 $build_dir/preflight-sha256.txt)"
-sed -i "s/PREFLIGHT_TAR_CHECKSUM/${tar_checksum}/g" $build_dir/preflight.yaml
+tvk_preflight_yaml="tvk-preflight.yaml"
+cp .krew/$tvk_preflight_yaml $build_dir/$tvk_preflight_yaml
+
+tvk_preflight_yaml=$build_dir/$tvk_preflight_yaml
+
+tar_checksum="$(awk '{print $1}' $build_dir/preflight-sha256.txt)"
+sed -i "s/PREFLIGHT_TAR_CHECKSUM/${tar_checksum}/g" $tvk_preflight_yaml
 echo >&2 "Written out preflight.yaml."
 
 echo >&2 "Creating Log-Collector plugin manifest yaml"
@@ -41,19 +45,22 @@ if hash sha256sum 2>/dev/null; then
   checksum_cmd="sha256sum"
 fi
 
-cp .krew/logCollector.yaml $build_dir/logCollector.yaml
+tvk_log_collector_yaml="tvk-log-collector.yaml"
+cp .krew/$tvk_log_collector_yaml $build_dir/$tvk_log_collector_yaml
+
+tvk_log_collector_yaml=$build_dir/$tvk_log_collector_yaml
 
 # shellcheck disable=SC2154
 log_collector_tar_archive="log-collector_${git_version}_linux_amd64.tar.gz"
 tar_checksum="$(eval "${checksum_cmd[@]}" "$build_dir/${log_collector_tar_archive}" | awk '{print $1;}')"
-sed -i "s/LOG_COLLECTOR_LINUX_TAR_CHECKSUM/${tar_checksum}/g" $build_dir/logCollector.yaml
+sed -i "s/LOG_COLLECTOR_LINUX_TAR_CHECKSUM/${tar_checksum}/g" $tvk_log_collector_yaml
 
 log_collector_tar_archive="log-collector_${git_version}_darwin_amd64.tar.gz"
 tar_checksum="$(eval "${checksum_cmd[@]}" "$build_dir/${log_collector_tar_archive}" | awk '{print $1;}')"
-sed -i "s/LOG_COLLECTOR_DARWIN_TAR_CHECKSUM/${tar_checksum}/g" $build_dir/logCollector.yaml
+sed -i "s/LOG_COLLECTOR_DARWIN_TAR_CHECKSUM/${tar_checksum}/g" $tvk_log_collector_yaml
 
 log_collector_tar_archive="log-collector_${git_version}_windows_amd64.zip"
 tar_checksum="$(eval "${checksum_cmd[@]}" "$build_dir/${log_collector_tar_archive}" | awk '{print $1;}')"
-sed -i "s/LOG_COLLECTOR_WINDOWS_TAR_CHECKSUM/${tar_checksum}/g" $build_dir/logCollector.yaml
+sed -i "s/LOG_COLLECTOR_WINDOWS_TAR_CHECKSUM/${tar_checksum}/g" $tvk_log_collector_yaml
 
 echo >&2 "Written out logCollector.yaml."
